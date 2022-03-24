6b55b3031d8739289b66739f6b72b6097e4f027d
Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
2022-03-24 09:18:20 -0500

    Merge develop (#43)
    * [ID-26] Add support for signature auth to retrieve access tokens (#31)
    
    * start adding function for getting access token from auth service using sigauth
    
    * add comment for sigauth get access token and update changelog
    
    * add GetAccessTokenRequest to remote data loader config
    
    * make access token request based on config setting
    
    * remove todos
    
    * add system claim (#41)
    
    * add first party claim (#42)
    
    Co-authored-by: Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Ryan Oberlander <46940735+roberlander2@users.noreply.github.com>
    Co-authored-by: Ryan Oberlander <reoberlander2@gmail.com>

--- authutils/utils.go
+++ authutils/utils.go
@@ -19,9 +19,11 @@ import (
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/json"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"net/http"
 )
 
 // ContainsString returns true if the provided value is in the provided slice
@@ -93,4 +95,31 @@ func HashSha256(data []byte) ([]byte, error) {
 		return nil, fmt.Errorf("error writing data: %v", err)
 	}
 	return hasher.Sum(nil), nil
+}
+
+// GetDefaultAccessTokenRequest returns a HTTP request to get an access token using a static token
+func GetDefaultAccessTokenRequest(host string, path string, token string) (*http.Request, error) {
+	if token == "" {
+		return nil, errors.New("service token is missing")
+	}
+
+	params := map[string]interface{}{
+		"auth_type": "static_token",
+		"creds": map[string]string{
+			"token": token,
+		},
+	}
+	data, err := json.Marshal(params)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling request body toget access token: %v", err)
+	}
+
+	r, err := http.NewRequest("POST", host+path, bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("error formatting request to get access token: %v", err)
+	}
+
+	r.Header.Set("Content-Type", "application/json")
+
+	return r, nil
 }
